f2f443819a879e0667f52f7413b79a552ee2663a
Johan Hartanto <70674862+johanhar88@users.noreply.github.com>
2024-01-26 15:09:38 +0700

    Summary
    - Add Member Button (Details)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/index.html', './src/index2.html', "./node_modules/flowbite/**/*.js"],
+  content: ['./src/index.html', './src/index2.html', './src/documents.html' ,"./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
