c1ab4dab200cdfcee8adf48412b1e744a5f0036a
Johan Hartanto <70674862+johanhar88@users.noreply.github.com>
2024-02-06 16:39:33 +0700

    Updates

--- src/td-dashboard.js
+++ src/td-dashboard.js
@@ -6,14 +6,3 @@ switcher.addEventListener('click', function () {
     switcher.checked ? html.classList.add('dark') : html.classList.remove('dark')
 })
 // END
-
-// Tab
-const btnConvo = document.getElementById('drill-thread')
-const profile = document.getElementById('profile-tab');
-const overview = document.getElementById('thread-tab');
-
-$(function () {
-    $(btnConvo).click(function () {
-        $(overview).prop('aria-selected',true);
-    });
-});
